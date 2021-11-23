55a220eb9b80be13fe1a2c9dcd220f81be5167a9
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-23 09:58:56 +0100

    create Arguments to manage easily args array

---
+++ src/main/java/fr/epsilonmc/api/command/Arguments.java
@@ -0,0 +1,12 @@
+package fr.epsilonmc.api.command;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Arguments {
+
+    private final String[] args;
+
+}
