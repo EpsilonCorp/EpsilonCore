32b5bcb610d66fc1ec5c576b76be5acd7d197d6d
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-23 10:00:33 +0100

    Create command registration

--- src/main/java/fr/epsilonmc/api/module/ModuleInfo.java
+++ src/main/java/fr/epsilonmc/api/module/ModuleInfo.java
@@ -1,10 +1,16 @@
 package fr.epsilonmc.api.module;
 
+import fr.epsilonmc.api.command.*;
+import fr.epsilonmc.api.exception.UnknownEpsilonCommandException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Getter
@@ -20,6 +26,28 @@ public class ModuleInfo {
             pluginManager.registerEvents(listener, plugin);
         }
 
-        // TODO: commands
+        for (IEpsilonCommand command : module.getCommands()) {
+            Optional<EpsilonCommand> optionalCommand = CommandFinder.findCommandOnClass(command);
+
+            if (optionalCommand.isPresent()) {
+                EpsilonCommand epsilonCommand = optionalCommand.get();
+                plugin.getCommand(epsilonCommand.name())
+                        .setExecutor((CommandSender bukkitSender, Command bukkitCommand, String label, String[] args) -> {
+                            Sender sender = new Sender(bukkitSender);
+                            Arguments arguments = new Arguments(args);
+                            // TODO: CommandInfo from plugin.yml
+                            CommandInfo commandInfo = new CommandInfo(
+                                    epsilonCommand.name(),
+                                    null,
+                                    null,
+                                    epsilonCommand.playerOnly()
+                            );
+
+                            return command.execute(sender, arguments, commandInfo);
+                        });
+            } else {
+                throw new UnknownEpsilonCommandException("Unknown command on %s!", command.getClass().getName());
+            }
+        }
     }
 }
