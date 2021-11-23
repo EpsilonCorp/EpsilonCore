8c03f9775d7e4c875db61d30c7c80eb726b64bef
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-23 10:00:21 +0100

    Add commands in module

--- src/main/java/fr/epsilonmc/api/module/AbstractModule.java
+++ src/main/java/fr/epsilonmc/api/module/AbstractModule.java
@@ -1,5 +1,6 @@
 package fr.epsilonmc.api.module;
 
+import fr.epsilonmc.api.command.IEpsilonCommand;
 import lombok.Getter;
 import org.bukkit.event.Listener;
 
@@ -7,4 +8,6 @@ public abstract class AbstractModule {
 
     @Getter private String name;
     @Getter private Listener[] listeners;
+    @Getter private IEpsilonCommand[] commands;
+
 }
