92f1038031131813fe67f21a682b391b4a8666e6
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-23 09:59:36 +0100

    Create EpsilonCommand annotation

---
+++ src/main/java/fr/epsilonmc/api/command/EpsilonCommand.java
@@ -0,0 +1,15 @@
+package fr.epsilonmc.api.command;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface EpsilonCommand {
+
+    String name();
+    boolean playerOnly() default false;
+
+}
