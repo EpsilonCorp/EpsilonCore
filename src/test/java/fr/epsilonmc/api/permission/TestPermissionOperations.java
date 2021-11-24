ff07bdb7ed29ea6fdaaa96f4e87c2161204f2409
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-24 18:41:21 +0100

    Create TestPermissionOperations.java

---
+++ src/test/java/fr/epsilonmc/api/permission/TestPermissionOperations.java
@@ -0,0 +1,39 @@
+package fr.epsilonmc.api.permission;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import fr.epsilonmc.core.Core;
+import fr.epsilonmc.core.Permissions;
+import fr.epsilonmc.mock.bukkit.EpsilonPlayerMock;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestPermissionOperations {
+
+    private ServerMock server;
+    private Core core;
+
+    @BeforeAll
+    public void setUp() {
+        server = MockBukkit.mock();
+        core = MockBukkit.load(Core.class);
+    }
+
+    @Test
+    @DisplayName("Test if validate method is working")
+    public void testValidate() {
+        EpsilonPlayerMock epsilonPlayerMock = new EpsilonPlayerMock(server, "Lucas__Lks");
+        epsilonPlayerMock.addAttachment(core, Permissions.PERMISSIONS_OP_PATTERN + ".test", true);
+
+        assertTrue(PermissionOperations.validate(epsilonPlayerMock, Permissions.PERMISSIONS_OP_PATTERN));
+    }
+
+
+    @AfterAll
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
+
+}
