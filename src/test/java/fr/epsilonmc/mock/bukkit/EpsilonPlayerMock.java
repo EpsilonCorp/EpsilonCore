456c6116ed76160c5f8731a2e64da65ff803a2db
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-24 18:43:02 +0100

    Create EpsilonPlayerMock to implement getEffectivePermoissions method

---
+++ src/test/java/fr/epsilonmc/mock/bukkit/EpsilonPlayerMock.java
@@ -0,0 +1,44 @@
+package fr.epsilonmc.mock.bukkit;
+
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class EpsilonPlayerMock extends PlayerMock {
+
+    public EpsilonPlayerMock(ServerMock server, String name) {
+        super(server, name);
+    }
+
+    @Override
+    public @NotNull Set<PermissionAttachmentInfo> getEffectivePermissions() {
+        try {
+            Field permissionAttachmentsField = getClass().getSuperclass().getSuperclass().getSuperclass().getDeclaredField("permissionAttachments");
+            permissionAttachmentsField.setAccessible(true);
+            return ((Set<PermissionAttachment>) permissionAttachmentsField.get(this))
+                    .stream()
+                    .map(permissionAttachment -> {
+                        List<String> permission = new ArrayList<>(permissionAttachment.getPermissions().keySet());
+
+                        return new PermissionAttachmentInfo(
+                                permissionAttachment.getPermissible(),
+                                permission.get(0),
+                                permissionAttachment,
+                                permissionAttachment.getPermissions().get(permission.get(0))
+                        );
+                    }).collect(Collectors.toSet());
+        } catch (NoSuchFieldException | IllegalAccessException exception) {
+//            exception.printStackTrace();
+        }
+        return Collections.emptySet();
+    }
+}
