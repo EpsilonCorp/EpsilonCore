cca43930e3685e102b6284a4333a88d4090d7455
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-24 13:33:37 +0100

    Add protected constructor for mocking Core

--- src/main/java/fr/epsilonmc/core/Core.java
+++ src/main/java/fr/epsilonmc/core/Core.java
@@ -4,11 +4,20 @@ import fr.epsilonmc.api.module.ModuleFactory;
 import fr.epsilonmc.core.modules.exp.ExpModule;
 import fr.epsilonmc.core.modules.secret.SecretModule;
 import lombok.Getter;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+
+import java.io.File;
 
 public final class Core extends JavaPlugin {
 
     @Getter private static Core instance;
+
+    // tests purposes only
+    public Core(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
+        super(loader, description, dataFolder, file);
+    }
 
     @Override
     public void onEnable() {
