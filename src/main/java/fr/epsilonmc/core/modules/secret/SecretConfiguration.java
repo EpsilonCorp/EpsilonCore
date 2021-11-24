7cce0c5ca43f52e8738625787774e23f2e8b52ec
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-24 17:52:12 +0100

    add default uuid for tests

--- src/main/java/fr/epsilonmc/core/modules/secret/SecretConfiguration.java
+++ src/main/java/fr/epsilonmc/core/modules/secret/SecretConfiguration.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class SecretConfiguration {
 
     @Getter private final String secretKey = "";
-    @Getter private final List<String> whoNeedsSecretLogin = new ArrayList<>();
+    @Getter private final List<String> whoNeedsSecretLogin = new ArrayList<String>(){{
+        add("253a3fe5-4bce-3192-bf99-6d0f5a0478d1"); // Tests purposes
+    }};
 
 }
