60b3b4955b117c3d56d6f8f054c60ec2a28a6ade
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-24 20:47:54 +0100

    Move SecretTest to SecretCacheTest and add secret key support

---
+++ src/test/java/fr/epsilonmc/core/modules/secret/SecretCacheTest.java
@@ -0,0 +1,77 @@
+package fr.epsilonmc.core.modules.secret;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import fr.epsilonmc.api.module.ModuleFactory;
+import fr.epsilonmc.api.thread.ThreadSafe;
+import fr.epsilonmc.core.Core;
+import fr.epsilonmc.mock.bukkit.EpsilonPlayerMock;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.RegisteredListener;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SecretCacheTest {
+
+    private ServerMock server;
+    private Core core;
+
+    @BeforeAll
+    public void setUp() {
+        server = MockBukkit.mock();
+        core = MockBukkit.load(Core.class);
+    }
+
+    @Test
+    @DisplayName("Test if Secret Cache works fine by using player quit")
+    public void testSecretCacheByQuit() {
+        SecretModule secretModule = ModuleFactory.getInstance().getModule(SecretModule.class).getModule();
+        EpsilonPlayerMock epsilonPlayerMock = new EpsilonPlayerMock(server, "Lucas__Lks");
+        epsilonPlayerMock.setOp(true);
+
+        PlayerLoginEvent playerLoginEvent = new PlayerLoginEvent(
+                epsilonPlayerMock,
+                "",
+                null,
+                PlayerLoginEvent.Result.ALLOWED,
+                ""
+        );
+        core.getServer().getPluginManager().callEvent(playerLoginEvent);
+        assertTrue(secretModule.getPlayerCache().contains("253a3fe5-4bce-3192-bf99-6d0f5a0478d1"));
+
+        PlayerQuitEvent playerQuitEvent = new PlayerQuitEvent(epsilonPlayerMock, "");
+        core.getServer().getPluginManager().callEvent(playerQuitEvent);
+        assertFalse(secretModule.getPlayerCache().contains("253a3fe5-4bce-3192-bf99-6d0f5a0478d1"));
+    }
+
+    @Test
+    @DisplayName("Test if Secret Cache works fine by using chat secret key")
+    public void testSecretCacheByChatSecretKey() {
+        SecretModule secretModule = ModuleFactory.getInstance().getModule(SecretModule.class).getModule();
+        EpsilonPlayerMock epsilonPlayerMock = new EpsilonPlayerMock(server, "Lucas__Lks");
+        epsilonPlayerMock.setOp(true);
+
+        PlayerLoginEvent playerLoginEvent = new PlayerLoginEvent(
+                epsilonPlayerMock,
+                "",
+                null,
+                PlayerLoginEvent.Result.ALLOWED,
+                ""
+        );
+        core.getServer().getPluginManager().callEvent(playerLoginEvent);
+        assertTrue(secretModule.getPlayerCache().contains("253a3fe5-4bce-3192-bf99-6d0f5a0478d1"));
+
+        epsilonPlayerMock.chat("/" + secretModule.getSecretConfiguration().getSecretKey());
+        ThreadSafe.sleep(10); // Because chat messages are sent via async threads
+        assertFalse(secretModule.getPlayerCache().contains("253a3fe5-4bce-3192-bf99-6d0f5a0478d1"));
+    }
+
+    @AfterAll
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
+}
