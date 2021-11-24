59934f88f196d4d0540d5925b97b6f0c23d79d1c
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-24 20:29:20 +0100

    Create TestModule.java

---
+++ src/test/java/fr/epsilonmc/api/module/TestModule.java
@@ -0,0 +1,25 @@
+package fr.epsilonmc.api.module;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestModule {
+
+    @Test
+    @DisplayName("Test if ModuleFinder works fine")
+    public void testModuleFinder() {
+        fr.epsilonmc.mock.core.modules.test.TestModule testModule = new fr.epsilonmc.mock.core.modules.test.TestModule();
+        EpsilonModule epsilonModule = ModuleFinder.findModuleOnClass(testModule);
+
+        assertNotNull(epsilonModule);
+        assertEquals("test", epsilonModule.name());
+        assertEquals(0, epsilonModule.listeners().length);
+        assertEquals(0, epsilonModule.commands().length);
+    }
+
+
+
+}
