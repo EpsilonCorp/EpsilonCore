48cc0d11758e0cb4c74a75b4a7b9e944b23de9d0
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-27 15:58:51 +0100

    create bin command

---
+++ src/test/java/fr/epsilonmc/core/modules/helper/TestBinCommand.java
@@ -0,0 +1,34 @@
+package fr.epsilonmc.core.modules.helper;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import fr.epsilonmc.api.io.ChatOperations;
+import fr.epsilonmc.core.Core;
+import fr.epsilonmc.mock.bukkit.EpsilonPlayerMock;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestBinCommand {
+
+    @BeforeAll
+    public void setUp() {
+        MockBukkit.mock();
+        MockBukkit.load(Core.class);
+    }
+
+    @Test
+    @DisplayName("Test if bin command works fine")
+    public void testBinCommand() {
+        EpsilonPlayerMock epsilonPlayerMock = new EpsilonPlayerMock("Lucas__Lks");
+        epsilonPlayerMock.performCommand("poubelle");
+
+        assertEquals(ChatOperations.translateColorCode("&8» Poubelle"), epsilonPlayerMock.getOpenInventory().getTitle());
+    }
+
+    @AfterAll
+    public void tearDown() {
+        MockBukkit.unload();
+    }
+
+}
