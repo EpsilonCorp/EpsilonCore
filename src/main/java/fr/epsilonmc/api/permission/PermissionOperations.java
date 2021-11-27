0ef9b7fc814822de605e87123eb2f44b8825150a
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-27 13:26:54 +0100

    Move to PermissionOperations#getHighestValueOfPattern

--- src/main/java/fr/epsilonmc/api/permission/PermissionOperations.java
+++ src/main/java/fr/epsilonmc/api/permission/PermissionOperations.java
@@ -3,7 +3,25 @@ package fr.epsilonmc.api.permission;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 
+import java.util.Set;
+
 public class PermissionOperations {
+
+    public static int getHighestValueOfPattern(Player player, String pattern) {
+        Set<PermissionAttachmentInfo> effectivePermissions = player.getEffectivePermissions();
+
+        int highest = -1;
+        for (PermissionAttachmentInfo effectivePermission : effectivePermissions) {
+            if (effectivePermission.getPermission().startsWith(pattern)) {
+                int multiplier = Integer.parseInt(effectivePermission.getPermission().substring(
+                        pattern.length()
+                ));
+                highest = Math.max(highest, multiplier);
+            }
+        }
+
+        return highest;
+    }
 
     public static boolean validate(Player player, String startWiths) {
         boolean isValid = player.isOp();
--- src/main/java/fr/epsilonmc/core/modules/exp/ExpCacheLoader.java
+++ src/main/java/fr/epsilonmc/core/modules/exp/ExpCacheLoader.java
@@ -1,28 +1,15 @@
 package fr.epsilonmc.core.modules.exp;
 
 import com.google.common.cache.CacheLoader;
+import fr.epsilonmc.api.permission.PermissionOperations;
 import fr.epsilonmc.core.Permissions;
 import org.bukkit.entity.Player;
-import org.bukkit.permissions.PermissionAttachmentInfo;
-
-import java.util.Set;
 
 public class ExpCacheLoader extends CacheLoader<Player, Integer> {
 
     @Override
     public Integer load(Player player) {
-        Set<PermissionAttachmentInfo> effectivePermissions = player.getEffectivePermissions();
-
-
-        int higherMultiplier = -1;
-        for (PermissionAttachmentInfo effectivePermission : effectivePermissions) {
-            if (effectivePermission.getPermission().startsWith(Permissions.EXP_MULTIPLIER_PATTERN)) {
-                int multiplier = Integer.parseInt(effectivePermission.getPermission().substring(
-                        Permissions.EXP_MULTIPLIER_PATTERN.length()
-                ));
-                higherMultiplier = Math.max(higherMultiplier, multiplier);
-            }
-        }
+        int higherMultiplier = PermissionOperations.getHighestValueOfPattern(player, Permissions.EXP_MULTIPLIER_PATTERN);
 
         return 100 + (higherMultiplier == -1 ? 0 : higherMultiplier);
     }
