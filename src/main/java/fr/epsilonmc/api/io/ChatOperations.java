593cead9292543fff2be5384cc3c6ccba72dd925
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-11-27 15:22:44 +0100

    Create translateColorCode for tests purposes

--- src/main/java/fr/epsilonmc/api/io/ChatOperations.java
+++ src/main/java/fr/epsilonmc/api/io/ChatOperations.java
@@ -1,14 +1,21 @@
 package fr.epsilonmc.api.io;
 
+import fr.epsilonmc.core.Variables;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class ChatOperations {
 
     public static void sendPrefixedAndTranslated(CommandSender commandSender, String txt, Object... objects) {
-        commandSender.sendMessage(String.format(
-                ChatColor.translateAlternateColorCodes('&', txt),
+        commandSender.sendMessage(translateColorCode(
+                Variables.CHAT_EPSILON_PREFIX + txt,
                 objects
+        ));
+    }
+
+    public static String translateColorCode(String txt, Object... objects) {
+        return ChatColor.translateAlternateColorCodes('&', String.format(
+                txt, objects
         ));
     }
 
