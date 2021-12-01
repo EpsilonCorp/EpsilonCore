89b0682d943e97be88ea5152e8f1fcfa3cd98683
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-12-01 17:21:01 +0100

    create prevent module

--- src/main/java/fr/epsilonmc/core/Variables.java
+++ src/main/java/fr/epsilonmc/core/Variables.java
@@ -5,6 +5,7 @@ import fr.epsilonmc.api.io.ChatOperations;
 public class Variables {
 
     public static final String MESSAGE_EPSILON_PREFIX = ChatOperations.translateColorCode("&6EpsilonMC &f&l» &f");
+    public static final String MESSAGE_PREVENT_ITEM_USE = ChatOperations.translateColorCode("&f&l» &cVous ne pouvez pas utiliser cet item.");
 
     public static final String DEATH_KILLS_COUNT = ChatOperations.translateColorCode("&fKill: &b");
 
---
+++ src/main/java/fr/epsilonmc/core/modules/prevent/PreventModule.java
@@ -0,0 +1,12 @@
+package fr.epsilonmc.core.modules.prevent;
+
+import fr.epsilonmc.api.module.EpsilonModule;
+import fr.epsilonmc.core.modules.prevent.listeners.ExploitFixerListener;
+
+@EpsilonModule(
+        name = "prevent",
+        listeners = {
+                ExploitFixerListener.class
+        })
+public class PreventModule {
+}
